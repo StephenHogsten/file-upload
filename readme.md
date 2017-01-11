ee70f02a3634fbd5673c06cf04e1e8d447eafef2
Hog Dog <throweymcthrowerson@gmail.com>
2017-01-11 22:11:05 +0000

    all done

--- readme.md
+++ readme.md
@@ -1,24 +1,8 @@
-Image Search API project
+File Upload API project
 
-Objective: Build a full stack JavaScript app that allows you to search for images like this: 
-https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/lolcats%20funny?offset=10 
-and browse recent search queries like this: 
-https://cryptic-ridge-9197.herokuapp.com/api/latest/imagesearch/. Then deploy it to Heroku.
+Objective: Build a full stack JavaScript app that is functionally similar to this: https://aryanj-file-size.herokuapp.com/ and deploy it to Heroku.
 
 
-User Story: I can get the image URLs, alt text and page urls for a set of images relating to a given search string.
+User Story: I can submit a FormData object that includes a file upload.
 
-User Story: I can paginate through the responses by adding a ?offset=2 parameter to the URL.
-
-User Story: I can get a list of the most recently submitted search strings.
-
-
-Some Resources:
-http://stackoverflow.com/questions/21530274/format-for-a-url-that-goes-to-google-image-search
-https://stenevang.wordpress.com/2013/02/22/google-advanced-power-search-url-request-parameters/
-http://jwebnet.net/advancedgooglesearch.html#advImageSearchURI
-http://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
-http://stackoverflow.com/questions/10209128/google-search-api-wrapper-for-node-js
-
-example query:
-https://www.google.com/search?hl=en&as_st=y&site=imghp&tbm=isch&q=lol%20cat
+User Story: When I submit something, I will receive the file size in bytes within the JSON response
--- server.js
+++ server.js
@@ -1,92 +1,17 @@
-var mongoclient = require('mongodb').MongoClient;
 var path = require('path');
 var express = require('express');
-var https = require('https');
-var url = require('url');
-var key = require('./api-key.js');
+var multer = require('multer');
+var upload = multer({dest: 'uploads'})
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/test', function(req, res){
-   res.redirect('test.html');
-   res.end();
-});
-
-app.get('/api/latest/imagesearch', function(req, res){ 
-   mongoclient.connect('mongodb://localhost:27017/appdata', function(err, db){
-      if (err) throw err;
-      var resultProm = db.collection('history').find({}, {_id:0}).sort({'when':-1}).limit(10).toArray();
-      resultProm.then( function(data) {
-          res.json(data);
-          res.end();
-          db.close();    
-      });
-   });
-});
-
-app.get('/api/imagesearch/:searchquery', function(req, res){
-    //save to mongodb
-    mongoclient.connect('mongodb://localhost:27017/appdata', function(err, db) {
-        if (err) throw err;
-        db.collection('history').insert({
-            term: req.params.searchquery,
-            when: new Date()
-        });
-        db.close();
-    });
-    
-    //format the api request for passing into google query
-    var searchquery = req.params.searchquery.replace(' ', '%20');
-    var query = url.parse(req.url, true).query;
-    var offset = (query.hasOwnProperty('offset'))? query.offset : 1;
-    var options = {
-        protocol: 'https:',
-        hostname: 'www.googleapis.com',
-        path: '/customsearch/v1?key=' + key.key + '&cx=004593805646146666066:0ys9didd2sq&q=' + searchquery + '&start=' + offset,
-        method: 'GET',
-        searchType: 'image'
-    };
-
-    var httpsReq = https.request(options, function(httpsRes){ 
-        var concatData = [];
-    
-        httpsRes.on('data', function(chunk){ 
-            concatData.push(chunk.toString());
-        })
-        
-        httpsRes.on('end', function(){
-            //put the response together and parse for our own return
-            var data = JSON.parse(concatData.join(''));
-            var results = [];
-            for (var i=0,l=data.items.length,d; i<l; i++) {
-                d = data.items[i];
-                if (!d.pagemap.hasOwnProperty('cse_image') || !d.pagemap.hasOwnProperty('cse_thumbnail')) continue;
-                results.push({
-                    title: data.items[i].title,
-                    url: data.items[i].pagemap.cse_image[0].src,
-                    snippet: data.items[i].snippet,
-                    thumbnail: data.items[i].pagemap.cse_thumbnail[0].src,
-                    context: data.items[i].link
-                });
-            }
-            res.json(results);
-            
-            res.end()
-        })
-    });
-    httpsReq.on('error', function(e){
-        console.log('error: ' + e);
-    })
-    httpsReq.end();
-})
-
-app.get('/*', function(req, res){
-    res.write('this is not a real place');
+app.post('/get-file-size', upload.single('filename'), function(req, res){
+    res.json({size: req.file.size});
     res.end();
-})
+});
 
 app.listen(8080, function(){
-    console.log('listener added');
+    console.log('new listener');
 });
